cfd3710d4163f14d1477dae0e16814e637605300
Abdellah Ramadan <ramadanabdel24@gmail.com>
2022-04-06 06:01:47 +0100

    progress on the new theme

--- assets/aurora-new.js
+++ assets/aurora-new.js
@@ -21,20 +21,20 @@ import './tiger/vendors/jquery-ui/jquery-ui.css';
 import './tiger/css/mibooz.css';
 import './tiger/css/mibooz-responsive.css'
 
-import jquery from 'jquery';
-window.jQuery, global.jQuery = jquery;
-//window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import $ from 'jquery';
+window.jQuery, global.jQuery = $;
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 
 import './tiger/vendors/bootstrap/js/bootstrap.bundle.min.js';
 import './tiger/vendors/jarallax/jarallax.min.js';
 import './tiger/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js';
 import './tiger/vendors/jquery-appear/jquery.appear.min.js';
 import './tiger/vendors/jquery-circle-progress/jquery.circle-progress.min.js';
-import './tiger/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js';
+//import './tiger/vendors/jquery-magnific-popup/jquery.magnific-popup.min.js';
 import './tiger/vendors/jquery-validate/jquery.validate.min.js';
 import './tiger/vendors/nouislider/nouislider.min.js';
 import './tiger/vendors/odometer/odometer.min.js';
-import './tiger/vendors/swiper/swiper.min.js';
+//import './tiger/vendors/swiper/swiper.min.js';
 import './tiger/vendors/tiny-slider/tiny-slider.min.js';
 import './tiger/vendors/wnumb/wNumb.min.js';
 import './tiger/vendors/wow/wow.js';
@@ -42,8 +42,8 @@ import './tiger/vendors/isotope/isotope.js';
 import './tiger/vendors/countdown/countdown.min.js';
 import './tiger/vendors/owl-carousel/owl.carousel.min.js';
 import './tiger/vendors/bxslider/jquery.bxslider.min.js';
-import './tiger/vendors/bootstrap-select/js/bootstrap-select.min.js';
+//import './tiger/vendors/bootstrap-select/js/bootstrap-select.min.js';
 import './tiger/vendors/jquery-ui/jquery-ui.js';
 import './tiger/vendors/jquery-tilt/tilt.jquery.min.js';
 
-import './tiger/js/mibooz.js';
+//import './tiger/js/mibooz.js';
